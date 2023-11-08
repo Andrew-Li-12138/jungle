f22c64a9b24208981ec43f3b147d14d26a0c91ec
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-11-07 23:14:43 -0500

    add validates to name for User Model and update class method authenticate_with_credentials so it allows spaces around email input and case insensitivty

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+  validates :name, presence: true
   has_secure_password
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by(email: email)
+    trimmed_lowercase_email = email.strip.downcase
+    User.update_all('email = lower(email)')
+    user = User.find_by(email: trimmed_lowercase_email)
     if user && user.authenticate(password)
       return user
     else
