15523fb9ec6d8ba5e99f6179858a5f57d522b53b
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-11-07 21:46:49 -0500

    create class method authenticate_with_credentials in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
+ 
