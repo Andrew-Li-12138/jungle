11b01138fa1f2dbc7b85033444d63d2c1a1c3d38
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-11-07 19:33:22 -0500

    unit tests for validation of attributes of Product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  describe 'Validations' do
-    # validation tests/examples here
-  end
+
+    it "is valid with all four fields(name, price, quantity, category) set" do
+      category = Category.create(name: "Test Category")
+      product = Product.new(
+        name: "Test Product",
+        price: 19.99,
+        quantity: 100,
+        category: category
+    )
+    expect(product.valid?).to be(true), "Errors: #{product.errors.full_messages.join(', ')}"
+end
+
+    it "is not valid without a name" do
+      category = Category.create(name: "Test Category")
+      product = Product.new(
+        price: 19.99,
+        quantity: 100,
+        category: category
+      )
+      expect(product.valid?).to be(false), "Errors: #{product.errors.full_messages.join(', ')}"
+    end
+
+    it "is not valid without a price" do
+      category = Category.create(name: "Test Category")
+      product = Product.new(
+      name: "Test Product",
+      quantity: 100,
+      category: category
+      )
+      expect(product.valid?).to be(false), "Errors: #{product.errors.full_messages.join(', ')}"
+    end
+    
+    it "is not valid without a quantity" do
+      category = Category.create(name: "Test Category")
+      product = Product.new(
+        name: "Test Product",
+        price: 19.99,
+        category: category
+      )
+      expect(product.valid?).to be(false), "Errors: #{product.errors.full_messages.join(', ')}"
+    end
+    
+    it "is not valid without a category" do
+      product = Product.new(
+        name: "Test Product",
+        price: 199.99,
+        quantity: 100
+      )
+      expect(product.valid?).to be(false), "Errors: #{product.errors.full_messages.join(', ')}"
+    end
+
 end
